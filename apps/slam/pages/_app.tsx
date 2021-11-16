868647be8a49b85b4eed0d8b9899aabe8fe19090
SDToews <33942967+SDToews@users.noreply.github.com>
2021-11-16 12:43:47 -0800

    project setup

---
+++ apps/slam/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import './styles.css';
+
+function CustomApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Welcome to slam!</title>
+      </Head>
+      <div className="app">
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default CustomApp;
---
+++ apps/slam/pages/index.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState, useCallback } from 'react';
+
+export function Index() {
+  const [state, setstate] = useState({});
+
+  const getSlam = useCallback(
+    async () => {
+      const response = await fetch('http://localhost:3333/api');
+      const body = await response.json()
+
+      setstate(body)
+    },
+    [],
+  )
+
+  useEffect(() => {
+    getSlam()
+
+  }, [])
+  return <div>{JSON.stringify(state)}</div>;
+}
+
+export default Index;
