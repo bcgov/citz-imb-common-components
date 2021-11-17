d24744079302b66ef8ca7cde148cb95f1f3220cf
SDToews <33942967+SDToews@users.noreply.github.com>
2021-11-17 13:12:57 -0800

    nest config

--- apps/api/slam/src/app/app.module.ts
+++ apps/api/slam/src/app/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: process.env.POSTGRES_SYNCHRONIZE === 'true',
+      logging: process.env.POSTGRES_LOGGING === 'true',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
