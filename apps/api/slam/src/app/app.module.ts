24fbd44a3a57bfaee337e9bb872e44d07f91e2d7
SDToews <33942967+SDToews@users.noreply.github.com>
2021-11-19 12:37:30 -0800

    update dockerfile

--- apps/api/slam/src/app/app.module.ts
+++ apps/api/slam/src/app/app.module.ts
@@ -8,18 +8,18 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(<string>process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      autoLoadEntities: true,
-      synchronize: process.env.POSTGRES_SYNCHRONIZE === 'true',
-      logging: process.env.POSTGRES_LOGGING === 'true',
-    }),
+    // ConfigModule.forRoot({ isGlobal: true }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: process.env.POSTGRES_HOST,
+    //   port: parseInt(<string>process.env.POSTGRES_PORT),
+    //   username: process.env.POSTGRES_USER,
+    //   password: process.env.POSTGRES_PASSWORD,
+    //   database: process.env.POSTGRES_DATABASE,
+    //   autoLoadEntities: true,
+    //   synchronize: process.env.POSTGRES_SYNCHRONIZE === 'true',
+    //   logging: process.env.POSTGRES_LOGGING === 'true',
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
